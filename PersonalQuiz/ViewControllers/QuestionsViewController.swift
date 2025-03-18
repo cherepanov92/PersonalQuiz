dde1e6b4cd7214974c01ea3f915e3c6f2450b0e6
Cherepanov Artyom <cherepanov92@gmail.com>
2025-03-18 23:30:30 +0500

    Подготовка QuestionVC, связь элементов

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -9,13 +9,42 @@ import UIKit
 
 final class QuestionsViewController: UIViewController {
     
+    // MARK: - IB Outlets
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    // MARK: - Private Properties
     private let questions = Question.getQuestions()
 
+    // MARK: - View Life Circles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - IB Actions
+    @IBAction func singleQuestionButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multipleQuestionButtonPressed() {
+    }
+    
+    @IBAction func rangedQuestionButtonPressed() {
+    }
 }
 
+// MARK: - Private Methods
+private extension QuestionsViewController {
+    
+}
